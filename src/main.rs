a95e191581acb4fc56b052d0b664d8dfa41c2a8e
Neil Pate <neil@premiergsolutions.com>
2023-11-07 23:31:53 +0100

    feat: Added toggling of LED blink rate depending on input switch

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,34 @@ pub use cortex_m_rt::entry;
 pub use panic_itm; // panic handler
 pub use stm32f3_discovery::stm32f3xx_hal; // Needed for the interrupt vector
 
+// Reset and Clock Control
 const RCC_ADDR: u32 = 0x4002_1000;
 const RCC_AHBENR_OFFSET: u32 = 0x14;
 const RCC_AHBENR: u32 = RCC_ADDR + RCC_AHBENR_OFFSET;
 
-const GPIOE_ADDR: u32 = 0x4800_1000;
-const GPIOE_BSRR_OFFSET: u32 = 0x18;
-const GPIOE_BSRR_ADDR: u32 = GPIOE_ADDR + GPIOE_BSRR_OFFSET;
-const GPIOE_MODER_OFFSET: u32 = 0x00;
-const GPIOE_MODER_ADDR: u32 = GPIOE_ADDR + GPIOE_MODER_OFFSET;
+// GPIO
 
-fn setup_gpio(pin: i32) -> () {
+const GPIO_BSRR_OFFSET: u32 = 0x18;
+const GPIO_MODER_OFFSET: u32 = 0x00;
+
+const GPIOA_ADDR: u32 = 0x4800_0000;
+const GPIOA_IDR: u32 = GPIOA_ADDR + 0x10;
+
+const GPIOE_ADDR: u32 = 0x4800_1000;
+const GPIOE_BSRR_ADDR: u32 = GPIOE_ADDR + GPIO_BSRR_OFFSET;
+const GPIOE_MODER_ADDR: u32 = GPIOE_ADDR + GPIO_MODER_OFFSET;
+
+fn setup_gpioa_pin_as_input() -> () {
+    unsafe {
+        // Enable the GPIOA peripheral
+        let ahbenr = &*(RCC_AHBENR as *mut volatile_register::RW<u32>);
+        ahbenr.modify(|r| r | (1 << 17));
+
+        // By default the pin is set to be an input, so no further config is needed
+    }
+}
+
+fn setup_gpioe_pin_as_output(pin: i32) -> () {
     unsafe {
         // Enable the GPIOE peripheral
         let ahbenr = &*(RCC_AHBENR as *mut volatile_register::RW<u32>);
@@ -54,21 +71,59 @@ fn short_delay(delay_ms: u16) {
     for _ in 0..(delay_ms * 67) {} // Constant experimentally determined
 }
 
+fn read_input() -> bool {
+    unsafe {
+        let idr = &*(GPIOA_IDR as *mut volatile_register::RW<u32>);
+        let value = idr.read();
+
+        let mask = 0x1;
+
+        value & mask > 0
+    }
+}
+
 fn blink_forever(pin: i32) -> ! {
-    let half_period_ms = 10_u16;
+    let fast_blink_half_period_ms = 100_u16;
+    let slow_blink_half_period_ms = fast_blink_half_period_ms / 2;
+    let mut blink_half_period_ms = fast_blink_half_period_ms;
+
+    let mut previous_switch_state = read_input();
+    let mut blinking_fast = true;
 
     loop {
+        // Sample the state of the switch, and if it transitions from low to high
+        // then switch to the other rate of blinking (fast/slow/fast/slow etc)
+
+        let switch_pressed = read_input();
+
+        if switch_pressed && !previous_switch_state {
+            // Toggle blinking rate
+
+            blinking_fast = !blinking_fast;
+
+            blink_half_period_ms = if blinking_fast {
+                fast_blink_half_period_ms
+            } else {
+                slow_blink_half_period_ms
+            };
+        }
+
+        previous_switch_state = switch_pressed;
+
         set_led_on(pin);
-        short_delay(half_period_ms);
+        short_delay(blink_half_period_ms);
 
         set_led_off(pin);
-        short_delay(half_period_ms);
+        short_delay(blink_half_period_ms);
     }
 }
 
 #[entry]
 fn main() -> ! {
-    let pin = 14;
-    setup_gpio(pin);
-    blink_forever(pin);
+    setup_gpioa_pin_as_input();
+
+    let led_output_pin = 13; // South direction of compass (Red)
+    setup_gpioe_pin_as_output(led_output_pin);
+
+    blink_forever(led_output_pin);
 }
